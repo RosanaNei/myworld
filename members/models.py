917a6a4749ea145c16b80b116d19578b6d9d0f5e
RosanaNei <rosageene@gmail.com>
2022-11-27 20:04:43 -0300

    merge

--- members/models.py
+++ members/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # esto es para crear la tabla en la base de datos
 class Members(models.Model):
-  nombre = models.CharField(max_length=255)
+  nombre = models.CharField(max_length=255, verbose_name= 'Nooombre')
   apellido = models.CharField(max_length=255)
   
 class Frutas(models.Model):
