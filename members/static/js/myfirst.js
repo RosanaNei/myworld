b9f18e8d308e772a00aa38dee0eee725b14c542b
RosanaNei <rosageene@gmail.com>
2022-11-29 16:16:16 -0300

    guardando

--- members/static/js/myfirst.js
+++ members/static/js/myfirst.js
@@ -1,3 +1,19 @@
 function myHalt() {
     alert("Hello from a static file!");
   }
+
+  const p = document.getElementById("libros"); // Encuentra el elemento de párrafo en la página a través de su id "foo"
+  p.onclick = mostrarAlerta; // Agrega la función onclick al elemento
+    
+  function mostrarAlerta(evento) {
+    alert("¡Evento onclick activado!");
+  }
+
+  /*function mostrarAlerta(evento){
+    var img = getElementById('libros');
+    if(img.className == 'libros'){
+      img.className = 'libros-grande';
+    }else{
+      img.className = 'libros';
+    }
+  }*/
